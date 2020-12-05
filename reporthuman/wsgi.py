4ce39499f1ded3ab3dcc829f884546cd8e487114
MinkwanJu <first-loving@hanmail.net>
2020-12-05 23:34:24 +0900

    Initial commit

---
+++ reporthuman/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for reporthuman project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "reporthuman.settings")
+
+application = get_wsgi_application()
