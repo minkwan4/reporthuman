730bcfdc25559c5bca7f6cdc7585d3621251d705
MinkwanJu <first-loving@hanmail.net>
2020-12-06 20:56:50 +0900

    django course 21 commit

--- reporthuman/settings.py
+++ reporthuman/settings.py
@@ -11,13 +11,11 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-
-import os, environ
+import environ
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
 )
-
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
