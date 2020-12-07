6b15a5a5a1e0bc9b6835b48bb286633be314a9ad
MinkwanJu <first-loving@hanmail.net>
2020-12-07 22:29:51 +0900

    django course 22 commit

--- reporthuman/settings.py
+++ reporthuman/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import environ
+from django.urls import reverse_lazy
+
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
@@ -135,3 +137,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR , "static"
 ]
+
+LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
+
+LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
