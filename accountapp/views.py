04dde2c30f0d979eacf79369133fddd1330a5dbe
MinkwanJu <first-loving@hanmail.net>
2020-12-12 20:59:03 +0900

    django coures 33 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -13,25 +13,24 @@ from accountapp.decorators import account_ownership_required
 from accountapp.forms import AccountUpdateForm
 from accountapp.models import HelloWorld
 
-has_ownership = [account_ownership_required, login_required ]
+has_ownership = [account_ownership_required, login_required]
 
 
 @login_required
 def hello_world(request):
+    if request.method == "POST":
 
-        if request.method == "POST":
+        temp = request.POST.get('hello_world_input')
 
-            temp = request.POST.get('hello_world_input')
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
 
-            new_hello_world = HelloWorld()
-            new_hello_world.text = temp
-            new_hello_world.save()
-
-            return HttpResponseRedirect(reverse('accountapp:hello_world'))
-        else:
-            hello_world_list = HelloWorld.objects.all()
-            return render(request, 'accountapp/hello_world.html',
-                        context={'hello_world_list': hello_world_list})
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
+    else:
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/hello_world.html',
+                      context={'hello_world_list': hello_world_list})
 
 
 class AccountCreateView(CreateView):
@@ -40,10 +39,12 @@ class AccountCreateView(CreateView):
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
 
+
 class AccountDetailView(DetailView):
     model = User
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
+
 
 @method_decorator(has_ownership, 'get')
 @method_decorator(has_ownership, 'post')
@@ -53,6 +54,7 @@ class AccountUpdateView(UpdateView):
     form_class = AccountUpdateForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
+
 
 @method_decorator(has_ownership, 'get')
 @method_decorator(has_ownership, 'post')
